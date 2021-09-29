706516a799423bb26d4fd3fc7b50f423a0f8b538
[Wilfred Owobu] <owobuwilfred@gmail.com>
2021-09-29 13:16:08 +0100

    added additional routes

--- runme.py
+++ runme.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import redirect,url_for
 
 weather_app = Flask(__name__)
 
 @weather_app.route("/")
 def index():
     return "<h1>Hello , Universe!</h1>"
+
+@weather_app.route("/home")
+def home():
+    
+    return "<h1>This is the home page!</h1> <a href = '/'>Return to Index</a>"
 
 
 
